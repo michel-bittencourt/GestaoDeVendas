c4fec01b279e81677d8e37155102dba95be13128
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2023-03-26 14:07:46 -0300

    Init

---
+++ GestaoDeVendas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoDeVendas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
